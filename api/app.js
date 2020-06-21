c2aa2bf919bdc4470e5395b10f3387cbdba01670
Chris Habgood <chabgood@gmail.com>
2020-06-21 00:50:49 -0500

    api init

---
+++ api/app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(morgan('dev'))
+app.use(bodyParser.json())
+
+app.use('/users', require('./routes/users'))
+app.listen(port)
+console.log(`server listening on port ${port}`)
