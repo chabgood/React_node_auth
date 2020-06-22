15da22405a70028f4684b3c59f3aac0635466d4f
Chris Habgood <chabgood@gmail.com>
2020-06-22 00:07:17 -0500

    pasport and auth

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -1,11 +1,39 @@
+const JWT = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+const User = require('../models/user')
+const { JWT_SECRET } = require('../configurations/index')
+
+signToken = (user) => {
+  return JWT.sign(
+    {
+      sub: user.insertId
+    },
+    JWT_SECRET,
+    { expiresIn: '1h' }
+  )
+}
+
 module.exports = {
   signUp: async (req, res, next) => {
-    console.log('UsersController.signUp called!')
+    const { email, password } = req.body
+    bcrypt.hash(password, 10, function (err, hash) {
+      User.query(
+        'insert into users set email = ?, password = ?',
+        [email, hash],
+        (error, user) => {
+          if (error) console.log('error' + error)
+          if (user) {
+            token = signToken(user)
+            res.status(200).json({ token })
+          }
+        }
+      )
+    })
   },
   signIn: async (req, res, next) => {
     console.log('UsersController.signIn called!')
   },
   secret: async (req, res, next) => {
-    console.log('UsersController.secret called!')
+    console.log('muy')
   }
 }
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,11 +1,19 @@
 const express = require('express')
 const router = require('express-promise-router')()
+const schemas = require('../helpers/routeHelpers')
+const passport = require('passport')
 const UsersController = require('../controllers/users')
+const middleware = require('../middleware/validations')
+const passportConf = require('../passport')
 
-router.route('/signup').post(UsersController.signUp)
+router
+  .route('/signUp')
+  .post(middleware(schemas.validateBody), UsersController.signUp)
 
-router.route('/signin').post(UsersController.signIn)
+router.route('/signIn').post(UsersController.signIn)
 
-router.route('/secret').get(UsersController.secret)
+router
+  .route('/secret')
+  .get(passport.authenticate('jwt', { session: false }), UsersController.secret)
 
 module.exports = router
