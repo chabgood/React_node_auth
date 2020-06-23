b19b59cdc68ff6aa130cd540b501bada67b09695
Chris Habgood <chabgood@gmail.com>
2020-06-22 22:36:59 -0500

    passport local

--- api/passport.js
+++ api/passport.js
@@ -1,19 +1,40 @@
 const passport = require('passport')
 const JwtStrategy = require('passport-jwt').Strategy
-const { ExtractJwt } = require('passport-jwt')
+const ExtractJwt = require('passport-jwt').ExtractJwt
+const LocalStrategy = require('passport-local').Strategy
 const { JWT_SECRET } = require('./configurations/index')
 const User = require('./models/user')
 
 passport.use(
   new JwtStrategy(
     {
-      jwtFromRequest: ExtractJwt.fromHeader('authorization'),
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken('Authorization'),
       secretOrKey: JWT_SECRET
     },
     async (payload, done) => {
       try {
-        const user = Article.query('SELECT * FROM users WHERE id = ? limit 1', [
+        const user = User.query('SELECT * FROM users WHERE id = ? limit 1', [
           payload.sub
+        ])
+        if (!user) {
+          return done(null, false)
+        }
+
+        done(null, user)
+      } catch (error) {}
+    }
+  )
+)
+
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: 'email'
+    },
+    async (emai, password, done) => {
+      try {
+        const user = User.query('SELECT * FROM users WHERE email = ? limit 1', [
+          email
         ])
         if (!user) {
           return done(null, false)
